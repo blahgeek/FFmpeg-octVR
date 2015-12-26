3ef7d9b30f86429d826eb3426a1a5757ac5f6566
BlahGeek <i@blahgeek.com>
2015-12-26 17:35:24 +0800

    support dongle license

--- config.bash
+++ config.bash
@@ -2,12 +2,14 @@
 
 export PKG_CONFIG_PATH=`pwd`/../lib/pkgconfig/
 OPENCV_INCLUDES=$(pkg-config --cflags opencv)
-OPENCV_LIBS=$(pkg-config --libs --static opencv)
+OPENCV_LIBS=$(pkg-config --libs --static opencv | sed 's/-L-L/-L/' | sed 's/-l64//')
+OPENSSL_INCLUDES=$(pkg-config --cflags openssl)
+OPENSSL_LIBS=$(pkg-config --libs openssl)
 PREFIX=`pwd`/../
 
 ./configure \
     --prefix="$PREFIX" \
-    --extra-cflags="-I../include $OPENCV_INCLUDES" \
-    --extra-libs="../lib/libmap.a $OPENCV_LIBS" \
+    --extra-cflags="-I../include $OPENCV_INCLUDES $OPENSSL_INCLUDES" \
+    --extra-libs="../lib/libmap.a ../lib/libDL.a $OPENCV_LIBS $OPENSSL_LIBS" \
     --extra-ldflags="-L/usr/local/cuda/lib64"
 
