5c49a8b5f7cd0912420d61fe3133c6a59f68779e
Feng Zhoutian <fengzhoutian@hotmail.com>
2016-02-21 16:22:49 +0800

    add config.win.bash (incomplete,  used in MSYS2 or other unix-shell environment under windows)

---
+++ config.win.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# FIXME: Though MSYS2 has pkg-config but OpenCV in Windows doesn't support pkg-config now.
+
+#export PKG_CONFIG_PATH=/usr/lib64/pkgconfig/:`pwd`/../lib/pkgconfig/
+#echo $PKG_CONFIG_PATH
+#OPENCV_INCLUDES=$(pkg-config --cflags opencv)
+#OPENCV_LIBS=$(pkg-config --libs --static opencv | sed 's/-L-L/-L/' | sed 's/-l64//')
+#OPENSSL_INCLUDES=$(pkg-config --cflags openssl)
+#OPENSSL_LIBS=$(pkg-config --libs openssl)
+PREFIX=`pwd`/../
+
+./configure \
+    --prefix="$PREFIX" \
+    --toolchain=msvc \
+    --extra-cflags="-I../include"
+#    --extra-cflags="-I../include $OPENCV_INCLUDES $OPENSSL_INCLUDES -I/home/blahgeek/nvidia_video_sdk_6.0.1/Samples/common/inc/ -I/opt/BlackmagicDeckLinkSDK/Linux/include" \
+#    --extra-libs="../lib/libdoge.a $OPENCV_LIBS $OPENSSL_LIBS" \
+#    --enable-gpl \
+#    --enable-libx264 \
+#    --enable-nonfree \
+#    --enable-nvenc \
+#    --enable-encoder=libx264,mpeg4 \
+#    --enable-decklink \
+#    --enable-indev=v4l2,dshow,decklink \
+#    --enable-outdev=v4l2,decklink \
+#    --extra-ldflags="-L/usr/local/cuda/lib64"
+
