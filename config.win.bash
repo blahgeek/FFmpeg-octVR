63a31be07abc2eb2d4e7ca6628eb66df05fcc4c6
Feng Zhoutian <fengzhoutian@hotmail.com>
2016-03-05 17:43:48 +0800

    A dirty hack for compiling with CUDA  because of whitespace in Windows's "Program Files" path.

--- config.win.bash
+++ config.win.bash
@@ -5,6 +5,7 @@
 PREFIX=`pwd`/../
 # In Windows, CUDA_PATH should be set when installing CUDA by default.
 CUDA_PATH=${CUDA_PATH//\\/\/}
+cp "${CUDA_PATH}"/lib/x64/*.lib .
 
 ./configure \
     --prefix="$PREFIX" \
@@ -18,4 +19,4 @@ CUDA_PATH=${CUDA_PATH//\\/\/}
     --enable-outdev=decklink \
     --extra-ldflags="/LIBPATH:\"C:/Qt/Qt5.5.1/5.5/msvc2013_64/lib\"" \
     --extra-cflags="-I../include -I../nvidia_video_sdk_6.0.1/Samples/common/inc -I../BlackmagicDeckLinkSDK/Win/include" \
-    --extra-libs="../lib/x64/pthreadVC2.dll ole32.lib user32.lib Qt5Core.lib ../x64/vc12/bin/*.dll \"${CUDA_PATH}\"/lib/x64/*.lib"
+    --extra-libs="../lib/x64/pthreadVC2.lib ole32.lib user32.lib Qt5Core.lib ../x64/vc12/lib/*.lib cublas.lib cublas_device.lib cuda.lib cudadevrt.lib cudart.lib cudart_static.lib cufft.lib cufftw.lib curand.lib cusolver.lib cusparse.lib nppc.lib nppi.lib npps.lib nvblas.lib nvcuvid.lib nvrtc.lib OpenCL.lib"
