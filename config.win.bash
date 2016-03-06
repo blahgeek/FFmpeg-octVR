797550ff87449726a9f3e3496504f2408d5ffb99
Feng Zhoutian <fengzhoutian@hotmail.com>
2016-03-06 20:38:06 +0800

    fix pthread header path. remove stdc++.lib when using MSVC toolchain

--- config.win.bash
+++ config.win.bash
@@ -18,5 +18,5 @@ cp "${CUDA_PATH}"/lib/x64/*.lib .
     --enable-indev=dshow,decklink \
     --enable-outdev=decklink \
     --extra-ldflags="/LIBPATH:\"C:/Qt/Qt5.5.1/5.5/msvc2013_64/lib\"" \
-    --extra-cflags="-I../include -I../nvidia_video_sdk_6.0.1/Samples/common/inc -I../BlackmagicDeckLinkSDK/Win/include" \
+    --extra-cflags="-I../include -I../include/pthread -I../nvidia_video_sdk_6.0.1/Samples/common/inc -I../BlackmagicDeckLinkSDK/Win/include" \
     --extra-libs="../lib/x64/pthreadVC2.lib ole32.lib user32.lib Qt5Core.lib ../x64/vc12/lib/*.lib cublas.lib cublas_device.lib cuda.lib cudadevrt.lib cudart.lib cudart_static.lib cufft.lib cufftw.lib curand.lib cusolver.lib cusparse.lib nppc.lib nppi.lib npps.lib nvblas.lib nvcuvid.lib nvrtc.lib OpenCL.lib"
