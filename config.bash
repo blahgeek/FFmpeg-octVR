91121d460e3b5c45ed546902b51fae35448982dd
Feng Zhoutian <fengzhoutian@hotmail.com>
2016-03-07 09:28:39 +0800

    Modify Linux configure script. Fix "=" operator in sh

--- config.bash
+++ config.bash
@@ -4,19 +4,16 @@ export PKG_CONFIG_PATH=/usr/lib64/pkgconfig/:`pwd`/../lib/pkgconfig/
 echo $PKG_CONFIG_PATH
 OPENCV_INCLUDES=$(pkg-config --cflags opencv)
 OPENCV_LIBS=$(pkg-config --libs --static opencv | sed 's/-L-L/-L/' | sed 's/-l64//' | sed 's/Qt5::Core/Qt5Core/')
-OPENSSL_INCLUDES=$(pkg-config --cflags openssl)
-OPENSSL_LIBS=$(pkg-config --libs openssl)
 PREFIX=`pwd`/../
 
 ./configure \
     --prefix="$PREFIX" \
-    --extra-cflags="-I../include $OPENCV_INCLUDES $OPENSSL_INCLUDES -I/home/blahgeek/nvidia_video_sdk_6.0.1/Samples/common/inc/ -I/opt/BlackmagicDeckLinkSDK/Linux/include" \
-    --extra-libs="$OPENCV_LIBS $OPENSSL_LIBS /opt/qt55/lib/libQt5Core.so ../lib/libdoge.a " \
+    --extra-cflags="-I../include $OPENCV_INCLUDES -I../nvidia_video_sdk_6.0.1/Samples/common/inc/ -I../BlackmagicDeckLinkSDK/Linux/include" \
+    --extra-libs="$OPENCV_LIBS /opt/qt55/lib/libQt5Core.so" \
     --enable-gpl \
-    --enable-libx264 \
     --enable-nonfree \
     --enable-nvenc \
-    --enable-encoder=libx264,mpeg4 \
+    --enable-encoder=mpeg4 \
     --enable-decklink \
     --enable-indev=v4l2,dshow,decklink \
     --enable-outdev=v4l2,decklink \
